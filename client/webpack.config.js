52a673bf4103b1aeccf6c4d3ae1efd7192adb6a0
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-11 16:56:33 -0700

    added transform-runtime

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -46,6 +46,7 @@ module.exports = () => {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              plugins: ["@babel/transform-runtime"],
             },
           },
         },
