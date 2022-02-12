59e5e567232e1d46c5c3f7d7e69866875a28485d
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-12 09:17:17 -0700

    got manifest.json to connect

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,7 +25,41 @@ module.exports = () => {
         title: "Webpack Plugin",
       }),
       new WebpackPwaManifest({
-        // TODO: Create a manifest.json:
+        short_name: "J.A.T.E ",
+        name: "Just Another Text Editor",
+        // icons: [
+        //   {
+        //     src: "./assets/images/icon_96x96.png",
+        //     type: "image/png",
+        //     sizes: "96x96",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "./assets/images/icon_128x128.png",
+        //     type: "image/png",
+        //     sizes: "128x128",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "./assets/images/icon_192x192.png",
+        //     type: "image/png",
+        //     sizes: "192x192",
+        //     purpose: "any maskable",
+        //   },
+        //   {
+        //     src: "./assets/images/icon_512x512.png",
+        //     type: "image/png",
+        //     sizes: "512x512",
+        //     purpose: "any maskable",
+        //   },
+        // ],
+        orientation: "portrait",
+        display: "standalone",
+        publicPath: "./",
+        start_url: "./",
+        description: "Takes notes with JavaScript syntax highlighting",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
